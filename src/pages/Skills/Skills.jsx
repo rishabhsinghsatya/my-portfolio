f50e18a651bea226a9326e7d7d6c3cdb761d1b07
Rishabh Singh <vineetsatya36@gmail.com>
2024-08-03 19:58:56 +0530

    adding skills sectio

---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Skills.css';
+
+const Skills = () => {
+  return (
+    <div className="skills-page">
+      {/* <h1>Skills</h1> */}
+      <div className="skills-list">
+      <button>HTML</button>
+      <button>CSS</button>
+      <button>Javascript</button>
+      <button>React Js</button>
+      <button>Node Js</button>
+      <button>Express Js</button>
+      <button>MongoDB</button>
+      <button>Git</button>
+      <button>Github</button>
+      <button>Docker</button>
+      <h1>. . .</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
